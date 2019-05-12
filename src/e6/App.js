616bcdb33a7f100610e3310090cca9cb9f7f204f
Megan Edwards <megmyfedwards@gmail.com>
2019-05-12 12:48:00 +0200

    Change to one function to save the input values

--- src/e6/App.js
+++ src/e6/App.js
@@ -13,34 +13,20 @@ class App extends React.Component {
         year: 1815
       }
     }
-    this.updateFirstName = this.updateFirstName.bind(this);
-    this.updateLastName = this.updateLastName.bind(this);
-    this.updateDateDay = this.updateDateDay.bind(this);
+    this.updateDetails = this.updateDetails.bind(this);
+    // this.updateFirstName = this.updateFirstName.bind(this);
+    // this.updateLastName = this.updateLastName.bind(this);
+    // this.updateDateDay = this.updateDateDay.bind(this);
   }
 
-  updateFirstName(event) {
+  updateDetails(event) {
     const currentField = event.currentTarget;
-    const input = currentField.value;
-    this.setState((prevState, props) => ({
-      firstName: prevState.firstName !== input ? input : prevState,      
-    }));
-  }
+    const key = currentField.id;
+    const newValue = {};
+    newValue[key] = currentField.value;
 
-  updateLastName(event) {
-    const currentField = event.currentTarget;
-    const input = currentField.value;
-    this.setState((prevState, props) => ({
-      lastName: prevState.lastName !== input ? input : prevState,
-    }));
-  }
-
-  updateDateDay(event) {
-    const currentField = event.currentTarget;
-    const input = currentField.value;
-    const day = this.state.birthday.day;
-    this.setState((prevState, props) => ({
-      day: prevState.day !== input ? input : prevState,
-    }));
+    console.log(newValue);
+    this.setState(newValue);
   }
 
   render() {
@@ -50,19 +36,19 @@ class App extends React.Component {
       <div className="container">
         <form className="form">
           <label htmlFor="firstName" className="input-label">First name</label>
-          <input id="firstName" className="input-box" placeholder={firstName} onChange={this.updateFirstName}/>
+          <input id="firstName" className="input-box" placeholder={firstName} onChange={this.updateDetails}/>
 
           <label htmlFor="lastName" className="input-label">Last name</label>
-          <input id="lastName" className="input-box" placeholder={lastName} onChange={this.updateLastName}/>
+          <input id="lastName" className="input-box" placeholder={lastName} onChange={this.updateDetails}/>
 
-          <label htmlFor="birthDate" className="input-label">Date of birth - Day</label>
-          <input id="birthDate" className="input-box" placeholder={birthDate.day} onChange={this.updateDateDay}/>
+          <label htmlFor="day" className="input-label">Date of birth - Day</label>
+          <input id="day" className="input-box" placeholder={birthDate.day} onChange={this.updateDetails}/>
 
-          <label htmlFor="birthDate" className="input-label">Date of birth - Month</label>
-          <input id="birthDate" className="input-box" placeholder={birthDate.month} />
+          <label htmlFor="month" className="input-label">Date of birth - Month</label>
+          <input id="month" className="input-box" placeholder={birthDate.month} onChange={this.updateDetails} />
 
-          <label htmlFor="birthDate" className="input-label">Date of birth - Year</label>
-          <input id="birthDate" className="input-box" placeholder={birthDate.year} />
+          <label htmlFor="year" className="input-label">Date of birth - Year</label>
+          <input id="year" className="input-box" placeholder={birthDate.year} onChange={this.updateDetails}/>
         </form>
 
         <ul className="data-list">
@@ -79,3 +65,28 @@ class App extends React.Component {
 }
 
 export default App;
+
+
+  // updateFirstName(event) {
+  //   const currentField = event.currentTarget;
+  //   const input = currentField.value;
+  //   this.setState((prevState, props) => ({
+  //     firstName: prevState.firstName !== input ? input : prevState,      
+  //   }));
+  // }
+
+  // updateLastName(event) {
+  //   const currentField = event.currentTarget;
+  //   const input = currentField.value;
+  //   this.setState((prevState, props) => ({
+  //     lastName: prevState.lastName !== input ? input : prevState,
+  //   }));
+  // }
+
+  // updateDateDay(event) {
+  //   const currentField = event.currentTarget;
+  //   const input = currentField.value;
+  //   const day = this.state.birthday.day;
+  //   this.setState((prevState, props) => ({
+  //     day: prevState.day !== input ? input : prevState,
+  //   }));
